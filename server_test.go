be1486d2e28279929a1f70e3a4a5a97e0dba4a82
Gabriel M <gmajivu@worldremit.com>
2024-01-15 12:59:40 +0300

    server: barebones server

---
+++ server_test.go
@@ -0,0 +1,26 @@
+package player_server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns score for a player", func(t *testing.T) {
+		// send api request
+		// record response
+		// assert expected
+		request, _ := http.NewRequest(http.MethodGet, "/players/bob", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
