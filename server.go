032b7d37240a6150fd4a0232083ab28bf6704a6c
Gabriel M <gmajivu@worldremit.com>
2024-01-15 16:56:48 +0300

    server: check games for Bryce
    - checking for another player games
    - this surfaces the need for routing
    - we had been mistakenly thinking about storage for player games and
      interfaces.
    - we resist the temptation to use any routing library; we do a simple
      `if` statement to pass our tests.

--- server.go
+++ server.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Bob" {
+		fmt.Fprint(w, "20")
+		return
+	}
+	if player == "Bryce" {
+		fmt.Fprint(w, "10")
+		return
+	}
 }
--- server_test.go
+++ server_test.go
@@ -7,17 +7,28 @@ import (
 )
 
 func TestGETPlayers(t *testing.T) {
-	t.Run("returns score for a player", func(t *testing.T) {
-		// send api request
-		// record response
-		// assert expected
-		request, _ := http.NewRequest(http.MethodGet, "/players/bob", nil)
+	t.Run("returns score for Bob", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Bob", nil)
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
 		got := response.Body.String()
 		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns score for player Bryce", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Bryce", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
