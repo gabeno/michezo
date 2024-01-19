9705e574319f006e3c6a6510d627baa181330bbc
Gabriel M <gmajivu@worldremit.com>
2024-01-19 13:11:02 +0300

    cli: record win for player

--- cli.go
+++ cli.go
@@ -1,9 +1,12 @@
 package poker
 
+import "io"
+
 type Cli struct {
 	store PlayerStore
+	in    io.Reader
 }
 
 func (c *Cli) PlayPoker() {
-	c.store.RecordWin("Cleo")
+	c.store.RecordWin("Chris")
 }
--- cli_test.go
+++ cli_test.go
@@ -1,16 +1,27 @@
 package poker
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCli(t *testing.T) {
 	t.Run("record a win for a player", func(t *testing.T) {
+		in := strings.NewReader("Chris /n")
 		playerStore := &StubPlayerStore{}
 
-		cli := &Cli{playerStore}
+		cli := &Cli{playerStore, in}
 		cli.PlayPoker()
 
 		if len(playerStore.winCalls) != 1 {
 			t.Fatal("expected a win call but did not get any")
+		}
+
+		got := playerStore.winCalls[0]
+		want := "Chris"
+
+		if got != want {
+			t.Errorf("did not record the correct winner, got %s want %s", got, want)
 		}
 	})
 }
